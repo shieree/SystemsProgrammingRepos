69eb2ee09898389b9afe91668c798c37f5d3ded6
Prometheseus <110165123+Prometheseus@users.noreply.github.com>
2023-02-22 03:39:02 -0500

    Add files via upload

--- mymalloc.c
+++ mymalloc.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 static char memory[4096];
 
 // Takes in a metadata number and converts the metadata's associated size to an int
@@ -7,7 +10,7 @@ int metadataSizeToIntSize(int metadata) {
 // Takes in an int size and the metadata location and places the size into the metadata
 void intSizeToMetadataSize(int intSize, int metadata) {
     int thousand = intSize / 1000;
-    intSize -= thousand*1000;
+    intSize -= thousand*1000;    
     int hundred = intSize / 100;
     intSize -= hundred*100;
     int tensAndOnes = intSize;
@@ -47,8 +50,12 @@ void combination() {
             printf("Error in combination(): invalid metadata found at %d.\n", nextMetadata);
             return;
         }
+
         // This runs if two adjacent chunks are free, combining them and setting the nextMetadata value
-        if (memory[prevMetadata+1] == 0 || -1 && memory[nextMetadata+1] == 0 || -1) {
+        if ((memory[prevMetadata+1] == 0 || memory[prevMetadata+1] == -1) && (memory[nextMetadata+1] == 0 || memory[nextMetadata+1] == -1)) {
+
+            printf("Combination(): Joining two free chunks! at prevMetadata %d and nextMetadata %d\n", prevMetadata, nextMetadata); // debugging print line
+
             int newChunkSize = metadataSizeToIntSize(prevMetadata) + metadataSizeToIntSize(nextMetadata) + 6;
             for (int i = 0; i < 6; i++) {
                 memory[nextMetadata+i] = 0;
@@ -63,11 +70,17 @@ void combination() {
         }
     }
 }
+// Function that erases the entire array for debugging purposes
+void eraseAll() {
+    for (int i = 0; i < 4096; i++) {
+        memory[i] = 0;
+    }
+}
 
-void *mymalloc (int size, char *file, int line) {
+void *mymalloc (size_t size, char *file, int line) {
     
     // Initialization that should only run once the first time malloc is called
-    if (memory[1] == 0 && memory[2] == 0 && memory[3] == 0 & memory[4] == 0) {
+    if (memory[1] == 0 && memory[2] == 0 && memory[3] == 0 && memory[4] == 0) {
         memory[0] = '~';
         memory[2] = 4;
         memory[4] = 90;
@@ -105,7 +118,10 @@ void *mymalloc (int size, char *file, int line) {
             exit(1);
         }
         // This runs if a free chunk of a sufficient size has been found
-        else if (memory[metadata+1] == 0 || -1 && metadataSizeInt-6 >= size) {
+        else if ((memory[metadata+1] == 0 || -1) && (metadataSizeInt-6 >= (int) size)) {
+
+            printf("----------\nmymalloc(): Found suitable chunk at metadata %d\n", metadata); // debugging print line
+
             foundSuitableChunk = 1;
             int chunkLeftOverSize = metadataSizeInt - size;
             memory[metadata+1] = 1;
@@ -120,65 +136,70 @@ void *mymalloc (int size, char *file, int line) {
                 intSizeToMetadataSize(chunkLeftOverSize-6, metadata+6+size);
                 memory[metadata+6+size+5] = '~';
             }
-        
+
+            printf("mymalloc(): Returning pointer to memory[%d]\n", metadata+6); // dDebugging print line
+
             return returnPointer;
         }
         // This runs if the current chunk is already occupied or if it is unoccupied and of insufficient size
         // Goes to next chunk
-        else if (memory[metadata+1] == 1 || (memory[metadata+1] == 0 || -1 && metadataSizeInt < size)) {
+        else if (memory[metadata+1] == 1 || ((memory[metadata+1] == 0 || -1) && metadataSizeInt < (int) size)) {
             metadata = metadata + metadataSizeInt + 6;
         }
         else {
             printf("Error in mymalloc(): inside the while loop.\n");
-            return;
+            exit(1);
         }
     }
 }
 
 void myfree(void* ptr, char *file, int line) {
 
-    // casts ptr to a char pointer 'target' for use in myfree()
+    // Casts ptr to a char pointer 'target' for use in myfree()
     char * target = (char *)ptr;
 
+    // Checking if the given pointer actually points to the memory array
     int validPointer = 0;
     for (int i = 0; i < 4096; i++) {
-        if (ptr == memory[i]) {
+        if (ptr == &memory[i]) {
             validPointer = 1;
             break;
         }
     }
-
     if (validPointer == 0) {
         printf("Error in myfree(): Not a valid pointer. File: %s, Line: %d\n", file, line);
         return;
     }
 
-    // successful use of myfree
+    // If metadata is valid, then free and call combination()
     if (target[-1] == '~' && target[-6] == '~' && target[-5] == 1 ) {
         target[-5] = -1;
-        printf("Chunk successfully freed!\n");
+
+        printf("myfree(): Chunk successfully freed!\n"); // debugging print line
+
+        combination();
         return;
     }
     
-    // pointer attempting to free a chunk already freed
+    // Pointer attempting to free a chunk already freed
     else if (target[-1] == '~' && target[-6] == '~' && target[-5] == -1) {
         printf("Error in myfree(): This chunk has already been freed. File: %s, Line: %d\n", file, line);
         return;
     }
 
-    // pointer attempting to free a chunk that is empty and never allocated
+    // Pointer attempting to free a chunk that is empty and never allocated
     else if (target[-1] == '~' && target[-6] == '~' && target[-5] == 0) {
         printf("Error in myfree(): This chunk was never allocated. File: %s, Line: %d\n", file, line);
         return;
     }
 
-    // pointer attempting to free a chunk with a pointer not at the start of a chunk
+    // Pointer attempting to free a chunk with a pointer not at the start of a chunk
     else if (target[-1] != '~' || target[-6] != '~') {
         printf("Error in myfree(): Pointer selected not at the start of a chunk. File: %s, Line: %d\n", file, line);
         return;
     }
 
-    // catch all invalid pointer for any other cases
+    // Catch all invalid pointer for any other cases
     else {
         printf("Error in myfree(): Not a valid pointer. File: %s, Line: %d\n", file, line);
     }
